9737ba0eb2cb5ffb2408ad463717f025f1665fdb
Erick Barrios <erickbarrios3@gmail.com>
2025-07-18 20:57:50 -0600

    Se cierra bimestre

--- cursos_extra/introduction-portfolio-construction-python/notebooks_and_codem01_v02/nb/edhec_risk_kit.py
+++ cursos_extra/introduction-portfolio-construction-python/notebooks_and_codem01_v02/nb/edhec_risk_kit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import scipy.stats
+import numpy as np
 
 def drawdown(return_series: pd.Series):
     """Takes a time series of asset returns.
@@ -72,3 +73,68 @@ def is_normal(r, level=0.01):
     else:
         statistic, p_value = scipy.stats.jarque_bera(r)
         return p_value > level
+    
+
+def semideviation(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a Series or a DataFrame, else raises a TypeError
+    """
+    is_negative = r < 0
+    return r[is_negative].std(ddof=0)
+
+def var_historic(r, level=5):
+    """
+    Returns the historic Value at Risk at a specified level
+    i.e. returns the number such that "level" percent of the returns
+    fall below that number, and the (100-level) percent are above
+    """
+    if isinstance(r, pd.DataFrame):
+        return r.aggregate(var_historic, level=level)
+    elif isinstance(r, pd.Series):
+        return -np.percentile(r, level)
+    else:
+        raise TypeError("Expected r to be a Series or DataFrame")
+    
+def cvar_historic(r, level=5):
+    """
+    Computes the Conditional VaR of Series or DataFrame
+    """
+    if isinstance(r, pd.Series):
+        is_beyond = r <= -var_historic(r, level=level)
+        return -r[is_beyond].mean()
+    elif isinstance(r, pd.DataFrame):
+        return r.aggregate(cvar_historic, level=level)
+    else:
+        raise TypeError("Expected r to be a Series or DataFrame")
+    
+from scipy.stats import norm
+def var_gaussian(r, level=5, modified=False):
+    """
+    Returns the Parametric Gauusian VaR of a Series or DataFrame
+    If "modified" is True, then the modified VaR is returned,
+    using the Cornish-Fisher modification
+    """
+    # compute the Z score assuming it was Gaussian
+    z = norm.ppf(level/100)
+    if modified:
+        # modify the Z score based on observed skewness and kurtosis
+        s = skewness(r)
+        k = kurtosis(r)
+        z = (z +
+                (z**2 - 1)*s/6 +
+                (z**3 -3*z)*(k-3)/24 -
+                (2*z**3 - 5*z)*(s**2)/36
+            )
+    return -(r.mean() + z*r.std(ddof=0))
+
+def semideviation3(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a Series or a DataFrame, else raises a TypeError
+    """
+    excess= r-r.mean()                                        # We demean the returns
+    excess_negative = excess[excess<0]                        # We take only the returns below the mean
+    excess_negative_square = excess_negative**2               # We square the demeaned returns below the mean
+    n_negative = (excess<0).sum()                             # number of returns under the mean
+    return (excess_negative_square.sum()/n_negative)**0.5     # semideviation
